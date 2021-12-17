fb3a74b26fd9c3dfb84675375af050d0993cb909
vienhoanglong <Vienhoanglong789@gmail.com>
2021-12-18 00:23:02 +0700

    add-employee-department-frontend

--- Project/Main.js
+++ Project/Main.js
@@ -1,1 +1,73 @@
 $(document).ready(function(){$('#sidebar-collapse').on('click',function(){$('#sidebar').toggleClass('active');});});
+// show dialog create employee
+$(document).ready(function () {
+    $(".click-create-employee").click(function () {
+        $('#create-employee').modal({
+            backdrop: 'static',
+            keyboard: false
+        });
+    });
+});
+// show dialog update employee
+$(document).ready(function () {
+    $(".click-update-employee").click(function () {
+        $('#update-employee').modal({
+            backdrop: 'static',
+            keyboard: false
+        });
+    });
+});
+// show dialog delete employee
+$(document).ready(function () {
+    $(".click-delete-employee").click(function () {
+        $('#delete-employee').modal({
+            backdrop: 'static',
+            keyboard: false
+        });
+    });
+});
+//show dialog re-password employee
+$(document).ready(function () {
+    $(".click-reset-password").click(function () {
+        $('#re-password-employee').modal({
+            backdrop: 'static',
+            keyboard: false
+        });
+    });
+});
+//show dialog create department
+$(document).ready(function () {
+    $(".click-create-department").click(function () {
+        $('#create-department').modal({
+            backdrop: 'static',
+            keyboard: false
+        });
+    });
+});
+//show dialog create promote
+$(document).ready(function () {
+    $(".click-create-promote").click(function () {
+        $('#create-promote').modal({
+            backdrop: 'static',
+            keyboard: false
+        });
+    });
+});
+// show dialog update department
+$(document).ready(function () {
+    $(".click-update-department").click(function () {
+        $('#update-department').modal({
+            backdrop: 'static',
+            keyboard: false
+        });
+    });
+});
+// show dialog delete employee
+$(document).ready(function () {
+    $(".click-delete-department").click(function () {
+        $('#delete-department').modal({
+            backdrop: 'static',
+            keyboard: false
+        });
+    });
+});
