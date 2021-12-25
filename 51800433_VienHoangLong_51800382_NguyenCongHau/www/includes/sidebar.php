2cac7384fe2420f238be022f0dfae43ec5465ec0
vienhoanglong <Vienhoanglong789@gmail.com>
2021-12-25 17:03:04 +0700

    Includes navbar sidebar, create change_password

---
+++ 51800433_VienHoangLong_51800382_NguyenCongHau/www/includes/sidebar.php
@@ -0,0 +1,29 @@
+<nav id="sidebar" class="active">
+    <div class="sidebar-header">
+        <h4>Manager Employee</h4>
+        <strong>ME</strong>
+    </div>
+    <ul class="components">
+        <li class="active">
+            <a href="#"><i class="fa fa-home"></i>Trang chủ</a>
+        </li>
+        <li>
+            <a href="#"><i class="fa fa-id-card-o"></i>Cá nhân</a>
+        </li>
+        <li>
+            <a href="#"> <i class="fa fa-users"></i>Quản lý nhân viên</a>
+        </li>
+        <li>
+            <a href="#"> <i class="fa fa-tasks"></i>Task</a>
+        </li>
+        <li>
+            <a href="#"> <i class="fa fa-briefcase"></i>Phòng ban</a>
+        </li>
+        <li>
+            <a href="#"><i class="fa fa-calendar"></i>Lịch</a>
+        </li>
+        <li>
+            <a href="#"><i class="fa fa-sign-out"></i>Thoát</a>
+        </li>
+    </ul>
+</nav>
